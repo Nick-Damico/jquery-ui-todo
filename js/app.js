d5422b8d8c1ce59fcf3c57d1576d486e17ff4b76
Nicholas D'Amico <nickalan82@icloud.com>
2018-03-06 23:23:55 -0500

    inital upload, fix input on hover annoying"

--- js/app.js
+++ js/app.js
@@ -1,5 +1,47 @@
 
-$(function() {
-$('.box').draggable();
+function isThirteen(keyCode) {
+    return keyCode === 13;
+}
 
-})
+$(function() {
+  const $todoLists = $('.todo--lists');
+  const $titleLi = $('li.title');
+  // Activate dropdown input
+   $todoLists.on({
+     mouseover:  function() {
+       $(this).children('li.input--li').slideDown(800);
+     },
+     mouseleave: function() {
+       $(this).children('li.input--li').slideUp(800);
+     }
+   })
+
+  // capture todo item and append to list
+  $('input').on('keypress', function(e) {
+    const $this = $(this);
+    const keyCode = (window.event) ? e.which : e.keyCode;
+    if ( isThirteen(keyCode) ) {
+      const inputVal = $this.val();
+      const liItem = `<li>${inputVal}</li>`;
+      $this.parent().parent().append(liItem);
+      $this.val('');
+    }
+  })
+
+  //  Make Lists Sortable
+  $todoLists.sortable({
+    items: "li:not('.title, input--li')",
+    connectWith: 'ul',
+    dropOnEmpty: true,
+    placeholder: "emptySpace"
+  });
+
+  // Droppable, removes draggable item
+  $('.div--remove-items').droppable({
+    drop: function(event, ui) {
+      ui.draggable.remove();
+    }
+  })
+
+
+});
