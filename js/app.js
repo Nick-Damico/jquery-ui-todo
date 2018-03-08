be717d9ef94b0836af9ff9b2025bdfb0d3a51db2
Nicholas D'Amico <nickalan82@icloud.com>
2018-03-08 16:51:17 -0500

    components now using BEM naming structure

--- js/app.js
+++ js/app.js
@@ -4,16 +4,15 @@ function isThirteen(keyCode) {
 }
 
 $(function() {
-  const $todoLists = $('.todo--lists');
-  const $titleLi = $('li.title');
+  const $todoLists = $('.list-container__list');
 
   // capture todo item and append to list
-  $('input').on('keypress', function(e) {
+  $('.list-container__input').on('keypress', function(e) {
     const $this = $(this);
     const keyCode = (window.event) ? e.which : e.keyCode;
     if ( isThirteen(keyCode) ) {
       const inputVal = $this.val();
-      const liItem = `<li class="todo-item-text">${inputVal}</li>`;
+      const liItem = `<li class="list-container__text">${inputVal}</li>`;
       $this.parent().parent().append(liItem);
       $this.val('');
     }
@@ -21,14 +20,14 @@ $(function() {
 
   //  Make Lists Sortable
   $todoLists.sortable({
-    items: "li:not('.title, input--li')",
+    items: "li:not('.list-container__title, :nth-child(2)')",
     connectWith: 'ul',
     dropOnEmpty: true,
     placeholder: "emptySpace"
   });
 
   // Droppable, removes draggable item
-  $('.div--remove-items').droppable({
+  $('.removal').droppable({
     drop: function(event, ui) {
       ui.draggable.remove();
     }
